1d1d10a856b394161200d350a4a0532fb5b4f744
martyn pratt <martynjamespratt@gmail.com>
2024-06-21 12:04:47 +0100

    updated build to also work in pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,17 @@ source $(git rev-parse --show-toplevel)/scripts/common.sh
 NAME=$(cat ${RUN_DIR}/NAME)
 VERSION=$(cat ${RUN_DIR}/VERSION)
 BUILD_EXIT_FILE=/tmp/build_exit_code
-BUILD_OUTPUT=/tmp/build_output_${VERSION}
+BUILD_OUTPUT=/tmp/build_output
+
+if [[ -z $(which fail) ]]; then
+   docker build -t $NAME:$VERSION . 2>&1 | tee $BUILD_OUTPUT && awk -F 'exit code: ' '/exit code/{print $2}' $BUILD_OUTPUT > $BUILD_EXIT_FILE || exit 1
+else
+   unbuffer docker build -t $NAME:$VERSION . 2>&1 | tee $BUILD_OUTPUT && awk -F 'exit code: ' '/exit code/{print $2}' $BUILD_OUTPUT > $BUILD_EXIT_FILE || exit 1
+fi
 
 #Retain colour in docker build and capture exit status
-unbuffer docker build -t $NAME:$VERSION . 2>&1 | tee $BUILD_OUTPUT && awk -F 'exit code: ' '/exit code/{print $2}' $BUILD_OUTPUT > $BUILD_EXIT_FILE || exit 1
+echo $CMD
+$CMD
 
 echo docker run $NAME:$VERSION
 echo -n exiting $(cat ${BUILD_EXIT_FILE})
