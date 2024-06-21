8a0debe4997c75dde9ebc64815c40560b0b36b33
martyn pratt <martynjamespratt@gmail.com>
2024-06-21 12:06:43 +0100

    removed unnecessary build stuff

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,13 +8,10 @@ BUILD_OUTPUT=/tmp/build_output
 if [[ -z $(which fail) ]]; then
    docker build -t $NAME:$VERSION . 2>&1 | tee $BUILD_OUTPUT && awk -F 'exit code: ' '/exit code/{print $2}' $BUILD_OUTPUT > $BUILD_EXIT_FILE || exit 1
 else
+   #Retain colour in docker build and capture exit status
    unbuffer docker build -t $NAME:$VERSION . 2>&1 | tee $BUILD_OUTPUT && awk -F 'exit code: ' '/exit code/{print $2}' $BUILD_OUTPUT > $BUILD_EXIT_FILE || exit 1
 fi
 
-#Retain colour in docker build and capture exit status
-echo $CMD
-$CMD
-
 echo docker run $NAME:$VERSION
 echo -n exiting $(cat ${BUILD_EXIT_FILE})
-exit $(cat $BUILD_EXIT_FILE && rm $BUILD_EXIT_FILE)
+exit $(cat $BUILD_EXIT_FILE)
